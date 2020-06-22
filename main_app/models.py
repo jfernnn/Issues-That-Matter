bc02bcc95366fe7e862951258dbb6619780ea015
April <macuser@Mac-User.local>
2020-06-22 14:37:34 -0600

    create models

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,33 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Resource(models.Model):
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    up_vote = models.ForeignKey(User, on_delete=CASCADE)
+    down_vote = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Comments(models.Model):
+    content = models.TextField(max_length=1000)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    user = models.CharField(max_length=150)
+        # get_user(request)
+
+class Topic(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for resource_id: {self.resource_id} @{self.url}"
