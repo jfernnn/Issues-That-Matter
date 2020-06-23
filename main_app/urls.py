e3a55391d54e1bcb26e2549e9a9e72aa1def7301
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-06-23 14:38:31 -0600

    remove pycachse

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('resources/<int:resource_id>/', views.resources_detail, name='detail'),
     path('resources/<int:pk>/update/', views.ResourceUpdate.as_view(), name='resources_update'),
     path('resources/<int:pk>/delete/', views.ResourceDelete.as_view(), name='resources_delete'),
-    
 ]
